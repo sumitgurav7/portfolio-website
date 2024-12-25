e32bb005f9f00e87ff4401bfe44820380b082e4e
sumit gurav <sumitgurav7@gmail.com>
2024-12-25 13:17:10 +0530

    updated project

--- portfolio-website/src/app/app.component.ts
+++ portfolio-website/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [ RouterLink, HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone:true
